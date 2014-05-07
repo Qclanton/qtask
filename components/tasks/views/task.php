b6c0a63c8195b8edd5e47946e70937b1727b436c
AlAyres <a.ayris@gmail.com>
2014-05-07 23:39:59 +0400

    Edit style for view task

--- components/tasks/views/task.php
+++ components/tasks/views/task.php
@@ -1,30 +1,93 @@
 <link rel="stylesheet" type="text/css" href="<?= $this->site_url; ?>components/tasks/views/css/task-style.css" />
 
 <div id="task--wrapper">
-		<h2><?= $task->title; ?></h2>
-		<? if (!empty($task->closed_date)) { ?>
-			Closed Date = <?= $task->closed_date; ?>
-		<? } ?>
-		<div class="task-details">
-		<h3>Details</h3>
-		<hr />
-		Creation Date: <?= $task->creation_date; ?>
-		<br />	
-		Project: <?= $task->project; ?>
-		<br />
-		Status: <?= $task->status; ?>
-		<br />
-		Priority: <?= $task->priority; ?>
-		<br />
-		Due date: <?= (!empty($task->due_date) ? $task->due_date : "&#8734"); ?>
-		<br />
+	<a class="edit-button" href="<?= $this->site_url; ?>index.php?component=tasks&action=showsetform&id=<?= $task->id; ?>">
+		<img title="Edit" class="edit-icon" src="<?= $this->site_url; ?>components/tasks/views/images/edit.png"></img>
+		<span>Edit Task</span>
+	</a>
+			
+	<h2><?= $task->title; ?></h2>
+	<? if (!empty($task->closed_date)) { ?>
+		Closed Date = <?= $task->closed_date; ?>
+	<? } ?>
+	<div class="task-details">
+		<h3>Details:</h3>
 
-		</div>
-		Text: <?= $task->text; ?>
-		<br />
-		Assigned to: <?= strtolower($task->assigned_type); ?> <?= $task->assigned; ?> 
-		<br />
-		<a href="<?= $this->site_url; ?>index.php?component=tasks&action=showsetform&id=<?= $task->id; ?>">
-			<button>Edit</button>
+		<table>
+			<tr>
+				<td class="first-col">
+				<span class="detail-name">Project:</span> 
+				</td>
+				<td>
+				<?= $task->project; ?>
+				</td>
+			</tr>
+			<tr>
+				<td class="first-col">
+				<span class="detail-name">Assigned to:</span> 
+				</td>
+				<td>
+				<?= strtolower($task->assigned_type); ?> <?= $task->assigned; ?> 
+				</td>
+			</tr>
+			<tr>
+				<td class="first-col">
+				<span class="detail-name">Status:</span> 
+				</td>
+				<td>
+				<?= $task->status; ?>
+				</td>
+			</tr>
+			<tr>
+				<td class="first-col">
+				<span class="detail-name">Priority:</span> 
+				</td>
+				<td>
+				<?= $task->priority; ?>
+				</td>
+			</tr>
+			<tr>
+				<td class="first-col">
+				<span class="detail-name">Creation Date:</span> 
+				</td>
+				<td>
+				<?= $task->creation_date; ?>
+				</td>
+			</tr>
+			<tr>
+				<td class="first-col">
+				<span class="detail-name">Due date:</span> 
+				</td>
+				<td>
+				<?= (!empty($task->due_date) ? $task->due_date : "&#8734"); ?>
+				</td>
+			</tr>
+		</table>
+	</div>
+	<h3>Description:</h3> 
+	<div id="task-description">
+		<?= $task->text; ?>
+	</div>
+	
+	<h3>Subtask:</h3> 
+	<div id="task-subtask">
+		<div id="task-subtask-button">
+
+		<a class="subtask-button" href="<?= $this->site_url; ?>index.php?component=tasks&action=showsetform&parent_task_id=<?= $task->id; ?>">
+			<img title="Create Subtask" class="plus-icon-sub" src="<?= $this->site_url; ?>components/tasks/views/images/plus.png"></img>
+			<span>Create Subtask</span>
 		</a>
+		</div>
+		<!-- List of subtasks -->
+		<? if (!empty($task->subtasks)) { ?>
+			<? foreach ($task->subtasks as $subtask) { ?>
+				<a href="<?= $this->site_url; ?>index.php?component=tasks&action=show&id=<?= $subtask->id; ?>"><?= $subtask->title; ?></a>
+			<? } ?>
+		<? } ?>
+	
+	</div>
+	
+	<? if (!empty($task->parent_task_id)) { ?>
+	<a href="<?= $this->site_url; ?>index.php?component=tasks&action=show&id=<?= $task->parent_task->id; ?>"><?= $task->parent_task->title; ?></a>
+	<? } ?>
 </div>

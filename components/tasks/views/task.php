cb672d436c507af5926ebdb406338e7df4b6d06c
AlAyres <a.ayris@gmail.com>
2014-05-10 03:00:32 +0400

    Few updates

--- components/tasks/views/task.php
+++ components/tasks/views/task.php
@@ -5,7 +5,7 @@
 		<img title="Edit" class="edit-icon" src="<?= $this->site_url; ?>components/tasks/views/images/edit.png"></img>
 		<span>Edit Task</span>
 	</a>
-			
+
 	<h2><?= $task->title; ?></h2>
 	<? if (!empty($task->closed_date)) { ?>
 		Closed Date = <?= $task->closed_date; ?>
@@ -84,10 +84,60 @@
 				<a href="<?= $this->site_url; ?>index.php?component=tasks&action=show&id=<?= $subtask->id; ?>"><?= $subtask->title; ?></a>
 			<? } ?>
 		<? } ?>
-	
+		
+		<!-- List of Tied Tasks -->
+		<? if (!empty($task->tied_tasks)) { ?>
+			<? foreach ($task->tied_tasks as $tied_task) { ?>
+				<? if ($tied_task->depended_object == "TASK") { ?>
+					This tasks depends of 
+				<? } elseif ($tied_task->depended_object == "TIED_TASK") { ?>
+					This tasks is source for
+				<? } ?>
+				
+				<a href="<?= $this->site_url; ?>index.php?component=tasks&action=show&id=<?= $tied_task->id; ?>"><?= $tied_task->title; ?></a>
+				(<a href="<?= $this->site_url; ?>index.php?component=tasks&action=untie&task_id=<?= $task->id; ?>&tied_task_id=<?= $tied_task->id; ?>&redirection_url=<?= urlencode($this->current_url); ?>">Untie</a>)
+			<? } ?>
+		<? } ?>
+		
+		<!-- Tie form -->
+		<a style="cursor:pointer;" id="show-tie_task-form-button">Show Tie Form</a>
+		<form style="display: none;" action="<?= $this->site_url; ?>index.php?component=tasks&action=tie" method="post" id="tie_task-form">
+			<input type="hidden" name="task_id" value="<?= $task->id; ?>"></input>
+			<input type="hidden" name="redirection_url" value="<?= $this->current_url; ?>"></input>
+			
+			
+			<select name="tied_task_id">
+				<? foreach ($tasks_for_tie as $task_for_tie) { ?>
+					<option value="<?= $task_for_tie->id; ?>"><?= $task_for_tie->id; ?>. <?= $task_for_tie->title; ?></option>
+				<? } ?>
+			</select>
+			
+			<select name="depended_object">
+				<option selected="selected" value="NONE">None</option>
+				<option value="TASK">This Task</option>
+				<option value="TIED_TASK">Tied Task</option>
+			</select>
+			
+			<input type="submit" value="Tie"></input>
+		</form>
+			
 	</div>
 	
 	<? if (!empty($task->parent_task_id)) { ?>
 	<a href="<?= $this->site_url; ?>index.php?component=tasks&action=show&id=<?= $task->parent_task->id; ?>"><?= $task->parent_task->title; ?></a>
 	<? } ?>
 </div>
+<script>	
+	$(function() {
+		$("#show-tie_task-form-button").on("click", function() {
+			if ($("#tie_task-form").hasClass("opened")) {
+				$("#tie_task-form").removeClass("opened").hide();
+				$("#show-tie_task-form-button").html("Show Tie Form");
+			}
+			else {
+				$("#tie_task-form").addClass("opened").show();
+				$("#show-tie_task-form-button").html("Hide Tie Form");
+			}
+		});
+	});	
+</script>
